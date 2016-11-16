bcb90c7674682709244fe66bc00392add4a52966
Kenny <savorycyanide@gmail.com>
2016-11-15 17:08:11 -0800

    Added beginning bits for game

--- base.js
+++ base.js
@@ -5,17 +5,17 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
     //Load background (Need image)
-    game.load.image()
+    game.load.image('sky', 'level_assets/temp_sky.png');
     //Load ground and platforms (Need image)
-    game.load.image()
+    game.load.image('ground', 'level_assets/temp_ground.png');
     //Load enemy (Need spritesheet)
-    game.load.spritesheet()
+    //game.load.spritesheet();
     //Load hero (Need spritesheet)
-    game.load.spritesheet()
+    game.load.spritesheet('hero', 'level_assets/temp_hero.png', 32, 48);
 }
 
 function create() {
-    
+    game.add.sprite(0, 0, 'hero');
 }
 
 function update() {
