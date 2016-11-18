861dd6198e71bb36e720c494b9fa62fc1c81e5a9
JavierLi <JavierLi@missionbits-MacBook-Air-10.local>
2016-11-17 16:26:05 -0800

    ayy lmao

--- base.js
+++ base.js
@@ -1,27 +1,28 @@
-//This is where things like movement and shooting is controlled
-//No idea if this works
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+    game.load.image('stars', 'http://wallup.net/wp-content/uploads/2016/01/171007-pixels-pixel_art-8-bit-moon-stars-video_games-space-dragon-clouds-cave_story.jpg');
+    //Load ground and platforms (Need image)
+    game.load.image('ground', '');
+    game.load.spritesheet('hero', 'https://phaser.io/content/tutorials/making-your-first-phaser-game/dude.png', 32, 48);
+}
+
+var s;
+var camSpeed = 4;
 var platforms;
 var player;
 
-function preload() {
-    //Load background (Need image)
-    game.load.image('sky', 'level_assets/temp_sky.png');
-    //Load ground and platforms (Need image)
-    game.load.image('ground', 'level_assets/temp_ground.png');
-    
-    //Load enemy (Need spritesheet)
-    //game.load.spritesheet();
-    
-    //Load hero (Need spritesheet)
-    game.load.spritesheet('hero', 'level_assets/temp_hero.png', 32, 48);
-}
-
 function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //  Make our world big ...
+    game.world.setBounds(0,0, 1920, 1080);
+
+    //  Scrolling background
+    s = game.add.tileSprite(0, 0, 1920, 1080, 'stars');
     
-    game.add.sprite(0, 0, 'sky');
+      game.add.sprite(0, 0, 'sky');
     //game.add.sprite(0, 0, 'hero');
     
     platforms = game.add.group();
@@ -40,10 +41,46 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
     
-    
 }
 
 function update() {
+ if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+    {
+        game.camera.x -= camSpeed;
+
+        if (!game.camera.atLimit.x)
+        {
+            s.tilePosition.x += camSpeed;
+        }
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
+    {
+        game.camera.x += camSpeed;
+
+        if (!game.camera.atLimit.x)
+        {
+            s.tilePosition.x -= camSpeed;
+        }
+    }
+
+    if (game.input.keyboard.isDown(Phaser.Keyboard.W))
+    {
+        game.camera.y -= camSpeed;
+
+        if (!game.camera.atLimit.y)
+        {
+            s.tilePosition.y += camSpeed;
+        }
+    }
+    else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
+    {
+        game.camera.y += camSpeed;
+
+        if (!game.camera.atLimit.y)
+        {
+            s.tilePosition.y -= camSpeed;
+        }
+    }
     game.physics.arcade.collide(player, platforms);
     
     player.body.velocity.x = 0;
@@ -78,5 +115,4 @@ function update() {
     {
         player.body.velocity.y = -350;
     }
-    
 }
